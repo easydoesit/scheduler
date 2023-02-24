6b44c5f396ce62562be07e2fdf5682b2ffc4a1f8
easydoesit <michael.grills@gmail.com>
2023-02-23 22:58:13 -0700

    add appointment booked and empty

--- src/components/Appointments/Form.jsx
+++ src/components/Appointments/Form.jsx
@@ -1,36 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 
 import InterviewerList from "components/InterviewerList.jsx";
 import Button from "components/Button.jsx";
 
 export default function Form(props) {
-  console.log(props);
+  console.log("these props: ", props);
+
+  const [student, setStudent] = useState(props.student || "");
+  const [interviewer, setInterviewer] = useState(props.interviewer || null);
+
+  const reset = () => {
+    setStudent("");
+    setInterviewer(null);
+  }
+
+  const cancel = () => {
+    reset();
+    props.onCancel();
+  }
+
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
-        <form autoComplete="off">
+        <form autoComplete="off" onSubmit={(event) => { event.preventDefault() }}>
           <input
             className="appointment__create-input text--semi-bold"
             name="name"
             type="text"
             placeholder="Enter student name"
-            value={props.student}
-          /*
-            This must be a controlled component
-            your code goes here
-          */
+            value={student}
+            onChange={(event) => {
+              setStudent(event.target.value);
+
+            }}
           />
         </form>
         <InterviewerList
           interviewers={props.interviewers}
-          interviewer={props.interviewer}
-        //setInterviewer={() => props.onChange(props.id)}
+          interviewer={interviewer}
+          onChange={setInterviewer}
         />
       </section>
       <section className="appointment__card-right">
         <section className="appointment__actions">
-          <Button danger onClick={props.onCancel}>Cancel</Button>
-          <Button confirm onClick={props.onSave(props.student, props.interviewer)}>Save</Button>
+          <Button danger onClick={cancel}>Cancel</Button>
+          <Button confirm onClick={() => props.onSave(student, interviewer)}>Save</Button>
         </section>
       </section>
     </main>
--- src/components/Appointments/index.js
+++ src/components/Appointments/index.js
@@ -1,8 +1,28 @@
 import React from "react";
 import "components/Appointments/styles.scss"
+import Header from "./Header.jsx";
+import Show from "./Show.jsx";
+import Empty from "./Empty.jsx";
 
 export default function Appointment(props) {
+  console.log("appointment: ", props);
+
+  const interview = {...props.interview};
+  
+  console.log("interview", interview); 
+
+  const checkInterviewer = () => {
+    if (interview.interviewer) {
+      return <Show  />
+    }
+    return <Empty />
+  }
+
+
   return(
-    <article className="appointment"></article>
+    <article className="appointment">
+      <Header time={props.time} />
+      {checkInterviewer()}
+    </article>
   );
 }
