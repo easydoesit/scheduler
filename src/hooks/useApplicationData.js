2e15427ffec5ee0bba38a47ba35819f690fb3e47
easydoesit <michael.grills@gmail.com>
2023-03-19 21:39:34 -0600

    move reducer function out of UseApplicationData into its own file

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -1,45 +1,12 @@
 import { useReducer, useEffect } from "react";
 import axios from 'axios';
 import { changeSpots } from "helpers/changeSpots";
+import reducer, { SET_DAY, SET_APPLICATION_DATA, SET_INTERVIEW } from "../reducers/application";
 
 export default function useApplicationData() {
   // this is the host machine
 
   const url = "ws://localhost:8001"
-
-  const SET_DAY = "SET_DAY";
-  const SET_APPLICATION_DATA = "SET_APPLICATION_DATA";
-  const SET_INTERVIEW = "SET_INTERVIEW";
-  // reducer brings in the original state and the action is an object
-  const reducer = (state, action) => {
-
-    switch (action.type) {
-      case SET_DAY:
-
-        return {
-          ...state,
-          day: action.day
-        }
-      case SET_APPLICATION_DATA:
-
-        return {
-          ...state,
-          days: action.days,
-          appointments: action.appointments,
-          interviewers: action.interviewers,
-        }
-      case SET_INTERVIEW:
-        return {
-          ...state,
-          appointments: action.appointments,
-          days: action.days
-        }
-      default:
-        throw new Error(
-          `Tried to reduce with unsupported action type: ${action.type}`
-        );
-    }
-  }
 
   // State object for the entire app
   const [state, dispatch] = useReducer(reducer, {
