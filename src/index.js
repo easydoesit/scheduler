41ee6df3e10c01719093ddc1e71b7e302fde9114
easydoesit <michael.grills@gmail.com>
2023-03-26 15:55:50 -0600

    remove console.logs

--- src/index.js
+++ src/index.js
@@ -10,7 +10,5 @@ import axios from "axios";
 if (process.env.REACT_APP_API_BASE_URL) {
   axios.defaults.baseURL = process.env.REACT_APP_API_BASE_URL;
 }
-console.log(process.env.REACT_APP_API_BASE_URL);
-
 
 ReactDOM.render(<Application />, document.getElementById("root"));
